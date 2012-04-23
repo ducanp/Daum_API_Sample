180b5643dae61dce7816ee5284d6e1c0cf5a43af
ducanp <ducanl12@gmail.com>
2012-04-23 15:20:44 +0900

    옵션메뉴 추가, URL 경로 임의로 바꿀 수 있도록 변경

--- KMLViewer/src/daum/kml/Example/KMLViewerActivity.java
+++ KMLViewer/src/daum/kml/Example/KMLViewerActivity.java
@@ -1,6 +1,7 @@
 package daum.kml.Example;
 
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 
@@ -8,13 +9,21 @@ import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapPolyline;
 import net.daum.mf.map.api.MapView;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class KMLViewerActivity extends Activity {
 	
 	private MapView mapView = null;
+	private EditText txtURL;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -24,28 +33,41 @@ public class KMLViewerActivity extends Activity {
         //MapAPI 초기화
         initMap();
         
-        try {
-        	/*
-        	 * KML 파일 경로
-        	 * web에 존재하는 파일 경로를 사용하거나
-        	 * local 영억에 존재하는 파일을 상용해도 된다.
-        	 * 단, 아래와 같이 프로토콜을 file://로 지정해 주어야 한다.
-        	 * */
-        	
-			//URL kmlPath = new URL("file:///sdcard/kml/c10c87be89cf3a79.kml");
-        	URL kmlPath = new URL("http://www.likejazz.com/kmz/z/2c286317b839c6d7.kml");
-        	//URL kmlPath = new URL("http://www.likejazz.com/kmz/z/c10c87be89cf3a79.kml");
-
-			InputStream kmlData = kmlPath.openStream();
+    	/*
+    	 * KML 파일 경로
+    	 * web에 존재하는 파일 경로를 사용하거나
+    	 * local 영억에 존재하는 파일을 상용해도 된다.
+    	 * 단, 아래와 같이 프로토콜을 file://로 지정해 주어야 한다.
+    	 * */
+    	
+    	try {
+    		//URL kmlPath = new URL("file:///sdcard/kml/c10c87be89cf3a79.kml");
+			URL kmlPath = new URL("http://www.likejazz.com/kmz/z/c10c87be89cf3a79.kml");
 			
-			//KML 데이터를 파싱, 선그리기
-			XMLParserWithPull parser = new XMLParserWithPull();
-			drawPolyLine( parser.xmlParserWithPull(kmlData) );
-			
-		} catch (Exception e) {
+			parseKML(kmlPath);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+    	
+    	
+
+		Toast.makeText(this, "KML 파일을 바꾸려면 메뉴를 누르세요.", Toast.LENGTH_SHORT).show();
     }
+	
+	/*
+	 * KML 데이터를 파싱, 선그리기
+	 * */
+	public void parseKML(URL kmlPath){
+		try{
+			InputStream kmlData = kmlPath.openStream();
+			
+			XMLParserWithPull parser = new XMLParserWithPull();
+			drawPolyLine( parser.xmlParserWithPull(kmlData) );
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 	
 	/*
 	 * KML Data 받아와서 선 그리기
@@ -53,7 +75,10 @@ public class KMLViewerActivity extends Activity {
 	public void drawPolyLine(ArrayList<CoordinatesData> coordinatesData){
 		try {
 			if(coordinatesData == null) return;
-						
+			
+			//맵에 있는 PolyLine 제거
+			mapView.removeAllPolylines();
+			
 			MapPolyline polyline = new MapPolyline();
 	        polyline.setLineColor(Color.argb(128, 0, 0, 255));
 	        
@@ -81,7 +106,9 @@ public class KMLViewerActivity extends Activity {
 			}
 				
 			//지도에 경로 추가
-			mapView.addPolyline(polyline);						
+			mapView.addPolyline(polyline);
+			
+			//경로에 맞춰서 지도 줌, 센터 자동으로 맞추기
 			mapView.fitMapViewAreaToShowAllPolylines();
 			
 		} catch (Exception e) {
@@ -105,7 +132,51 @@ public class KMLViewerActivity extends Activity {
         setContentView(mapView);
 	}
 	
-	public MapView getMapView() {
-		return mapView;
+	/* 
+	 * 옵션메뉴
+	 * return 	true - 화면에 옵션 메뉴를 보여줌
+	 * 			false - 화면에 옵션 메뉴를 보여주지 않음
+	 */
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.layout.main_option_menu, menu);
+
+		return true;
+	}
+	
+	/*
+	 * 옵션 메뉴 선택시에 발생하는 이벤트 처리 메소드
+	 */
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item){
+		if(item.getItemId() == R.id.input_url){
+			//URL 입력할 다이얼로그 띄우기
+			txtURL = new EditText(this);
+			txtURL.setText("http://www.likejazz.com/kmz/z/9e3c51ceb56e5c9c.kml");
+			
+			new AlertDialog.Builder(this)
+					.setTitle("URL을 입력하세요.")
+					.setView(txtURL)
+					.setPositiveButton("확인", new DialogInterface.OnClickListener()
+					{
+						@Override
+						public void onClick(DialogInterface dialog, int which)
+						{
+							try {
+								//입력받은 URL로 새로운 경로 표시
+								parseKML(new URL( txtURL.getText().toString() ));
+							} catch (MalformedURLException e) {
+								e.printStackTrace();
+							}
+						}
+					})
+					.setNegativeButton("취소", null)
+					.show();
+		}
+		
+		return true;
 	}
 }
